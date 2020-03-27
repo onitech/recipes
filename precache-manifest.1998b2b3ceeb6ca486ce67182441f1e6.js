4f1057b456442f4322db11d64ca65e7fbeeb12a9
onitech <mc.busante@gmail.com>
2020-03-28 04:18:26 +0800

    Updates

---
+++ precache-manifest.1998b2b3ceeb6ca486ce67182441f1e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f842ce3fe062755c68694a519e909b7",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "8ec5c146b00fdabbb78f",
+    "url": "/recipes/static/css/main.77a869a8.chunk.css"
+  },
+  {
+    "revision": "5fe15cfc40981bd82161",
+    "url": "/recipes/static/js/2.6f6baed7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.6f6baed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec5c146b00fdabbb78f",
+    "url": "/recipes/static/js/main.eb37cbfb.chunk.js"
+  },
+  {
+    "revision": "a1efb057f1635746220a",
+    "url": "/recipes/static/js/runtime-main.065fb63f.js"
+  }
+]);
