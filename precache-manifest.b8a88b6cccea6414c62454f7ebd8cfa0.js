1548f8ed53eddd2c38d4a2165e311ddde15248af
onitech <mc.busante@gmail.com>
2020-03-30 20:56:36 +0800

    Updates

---
+++ precache-manifest.b8a88b6cccea6414c62454f7ebd8cfa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7fcc71331e112b8791f0e54406ebb4",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "cd54ec1e6d29fee4b552",
+    "url": "/recipes/static/css/main.3c0a5d9f.chunk.css"
+  },
+  {
+    "revision": "5c0dd9169b3885c064d2",
+    "url": "/recipes/static/js/2.126a3a32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.126a3a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd54ec1e6d29fee4b552",
+    "url": "/recipes/static/js/main.f4dceba1.chunk.js"
+  },
+  {
+    "revision": "a1efb057f1635746220a",
+    "url": "/recipes/static/js/runtime-main.065fb63f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/recipes/precache-manifest.73b53ae4ad8fe420c69f192b648892e4.js"
+  "/recipes/precache-manifest.b8a88b6cccea6414c62454f7ebd8cfa0.js"
 );
 
 self.addEventListener('message', (event) => {
